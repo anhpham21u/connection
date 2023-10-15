f8eedfedd9b19954e1f259b2f793142511c76246
anhpham21u <hoctap31111@gmail.com>
2023-10-16 01:33:22 +0700

    add login and logout and register

--- connection/settings.py
+++ connection/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'app',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -106,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL ='authentication.Account'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
