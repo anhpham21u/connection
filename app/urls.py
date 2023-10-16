3e896aa02268d3c8a1cb40cc9b7999ca95ec89b1
anhpham21u <hoctap31111@gmail.com>
2023-10-17 05:47:33 +0700

    add create new post

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('newpost/', views.new_post, name="newpost"),
     path('post/<str:id>/', views.post, name="post")
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import PostForm
 from .models import Post
 
 # Create your views here.
@@ -12,3 +13,19 @@ def post(req, id):
     except Post.DoesNotExist:
         pass
     return render(req, 'app/post.html', {'post': post})
+
+def new_post(request):
+    user_log = request.user
+    if not user_log.is_authenticated:
+        return redirect('login')
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user  
+            post.save()
+            return redirect('index')
+    else:
+        form = PostForm()
+    return render(request, 'app/newpost.html', {'form': form})
