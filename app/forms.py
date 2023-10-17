4acd1f0e22d6d4c0cd30e8f7c25ce2b885c8d0ce
anhpham21u <hoctap31111@gmail.com>
2023-10-18 01:28:38 +0700

    edit bar in front

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from .models import Post
+from tinymce.widgets import TinyMCE
 
 class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
     class Meta:
         model = Post
         fields = ['title', 'content']
