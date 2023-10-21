07611096324b5c74c59be60f643eb6449c561ff9
anhpham21u <hoctap31111@gmail.com>
2023-10-21 08:36:19 +0700

    create Topic model

--- app/models.py
+++ app/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -9,6 +16,7 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     likes = models.ManyToManyField(User, related_name='liked_posts')
     pub_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.title
